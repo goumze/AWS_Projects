0b6750734189fd59920a17ba44686a71f6900b1e
Goutam <gm_mishra@hotmail.com>
2021-02-06 12:34:42 +0530

    Reference link for connectivity to an example of Oracle RDS.

--- src/main/java/com/test/aws/ch4/database/ConnectToRDS.java
+++ src/main/java/com/test/aws/ch4/database/ConnectToRDS.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 import static com.test.aws.ch4.database.RDSEndpointRetrieval.getdbInstance;
 
+//https://www.youtube.com/watch?v=HfukksuHU6Q&t=425s
 public class ConnectToRDS {
 
     public static void main(String...args) throws SQLException, ClassNotFoundException {
