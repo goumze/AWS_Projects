1983ec5d9f50c19aa8f0b6583d2b5cfb1d5c7579
goumze <gm_mishra@hotmail.com>
2021-07-21 22:51:22 +0530

    Changes implemented for demoing lambda events

---
+++ src/main/java/com/test/aws/lambda/examples/OrderHandler.java
@@ -0,0 +1,7 @@
+package com.test.aws.lambda.examples;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String,Object> {
+
+}
