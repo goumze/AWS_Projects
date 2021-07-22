e02c9727bcf570b72b1e888557c2a07c5d9b4986
goumze <gm_mishra@hotmail.com>
2021-07-22 23:48:44 +0530

    Keeping files only for Lambda test

--- src/main/java/com/test/aws/AWSSamplesStarter.java
+++ src/main/java/com/test/aws/AWSSamplesStarter.java
@@ -1,34 +1,20 @@
 package com.test.aws;
 
-import com.test.aws.lambda.examples.domain.Order;
-import com.test.aws.lambda.examples.repository.OrderDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.FunctionalSpringApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class AWSSamplesStarter {
 
-    @Autowired
-    private OrderDao orderDao;
-
     @Bean
-    public Supplier<List<Order>> orders() {
-        return () -> orderDao.buildOrders();
-    }
-
-    @Bean
-    public Function<String, List<Order>> findOrderByName() {
-        return (input) -> orderDao.buildOrders().stream().filter(order -> order.getName().equals(input)).collect(Collectors.toList());
+    public Function<String, String> uppercase() {
+        return value -> value.toUpperCase();
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(AWSSamplesStarter.class, args);
+        FunctionalSpringApplication.run(AWSSamplesStarter.class, args);
     }
 }
